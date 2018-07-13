5a3af1e81ce8e5705ac6bcf13eb4af081f0ee801
James Arnold <31871789+arnoldjames98@users.noreply.github.com>
2018-07-13 10:24:56 -0400

    Create GatewayConfiguration.java

---
+++ src/main/java/gov/nist/hla/samples/traffic/GatewayConfiguration.java
@@ -0,0 +1,42 @@
+package gov.nist.hla.samples.traffic;
+
+import gov.nist.hla.gateway.GatewayFederateConfig;
+
+public class GatewayConfiguration extends GatewayFederateConfig {
+    private String username = "root";
+    private String password = "root";
+    private String ipAddress = "localhost";
+    private String port = "3306";
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+    
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+    
+    public String getIpAddress() {
+        return this.ipAddress;
+    }
+    
+    public void setPort(String port) {
+        this.port = port;
+    }
+    
+    public String getPort() {
+        return this.port;
+    }
+}
